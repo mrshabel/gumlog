5dbd4e59bca9db4cc62b30ebef3c8d0a97100195
Shabel Gumah <sgumah001@st.ug.edu.gh>
2025-05-01 11:58:53 +0000

    feat: add server-to-server service discovery

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require github.com/gorilla/mux v1.8.1
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/Knetic/govaluate v3.0.1-0.20171022003610-9aa49832a739+incompatible // indirect
+	github.com/armon/go-metrics v0.4.1 // indirect
 	github.com/casbin/casbin v1.9.1 // indirect
 	github.com/cloudflare/cfssl v1.6.5 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
@@ -14,16 +15,28 @@ require (
 	github.com/go-sql-driver/mysql v1.9.2 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/google/btree v1.1.3 // indirect
 	github.com/google/certificate-transparency-go v1.3.1 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/grpc-ecosystem/go-grpc-middleware v1.4.0 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-immutable-radix v1.3.1 // indirect
+	github.com/hashicorp/go-metrics v0.5.4 // indirect
+	github.com/hashicorp/go-msgpack/v2 v2.1.2 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/hashicorp/go-sockaddr v1.0.5 // indirect
+	github.com/hashicorp/golang-lru v1.0.2 // indirect
+	github.com/hashicorp/memberlist v0.5.2 // indirect
+	github.com/hashicorp/serf v0.10.2 // indirect
 	github.com/jmhodges/clock v1.2.0 // indirect
 	github.com/jmoiron/sqlx v1.4.0 // indirect
 	github.com/kisielk/sqlstruct v0.0.0-20210630145711-dae28ed37023 // indirect
 	github.com/lib/pq v1.10.9 // indirect
 	github.com/mattn/go-sqlite3 v1.14.28 // indirect
+	github.com/miekg/dns v1.1.56 // indirect
 	github.com/pelletier/go-toml v1.9.5 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/sean-/seed v0.0.0-20170313163322-e2103e2c3529 // indirect
 	github.com/stretchr/testify v1.10.0 // indirect
 	github.com/tysonmote/gommap v0.0.3 // indirect
 	github.com/weppos/publicsuffix-go v0.40.3-0.20250408071509-6074bbe7fd39 // indirect
@@ -33,9 +46,12 @@ require (
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
 	golang.org/x/crypto v0.37.0 // indirect
+	golang.org/x/mod v0.22.0 // indirect
 	golang.org/x/net v0.39.0 // indirect
+	golang.org/x/sync v0.13.0 // indirect
 	golang.org/x/sys v0.32.0 // indirect
 	golang.org/x/text v0.24.0 // indirect
+	golang.org/x/tools v0.29.0 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250115164207-1a7da9e5054f // indirect
 	google.golang.org/grpc v1.71.1 // indirect
---
+++ internal/discovery/membership.go
@@ -0,0 +1,149 @@
+package discovery
+
+import (
+	"net"
+
+	"github.com/hashicorp/serf/serf"
+	"go.uber.org/zap"
+)
+
+// cluster membership definition for service discovery
+type Membership struct {
+	Config
+	handler Handler
+	serf    *serf.Serf
+	// entry and exist events channel
+	events chan serf.Event
+	// logger instance for service discovery activities
+	logger *zap.Logger
+}
+
+// New creates a new serf membership instance for the current node
+func New(handler Handler, config Config) (*Membership, error) {
+	c := &Membership{
+		Config:  config,
+		handler: handler,
+		logger:  zap.L().Named("membership"),
+	}
+	if err := c.setupSerf(); err != nil {
+		return nil, err
+	}
+	return c, nil
+}
+
+// configuration for current serf node
+type Config struct {
+	// unique name of the current node. defaults to its hostname
+	NodeName string
+	// address for gossiping
+	BindAddr string
+
+	// key value metadata tags to give more context about the node.
+	// can be used to shared info on whether a node is a voter or not,
+	// and RPC addresses
+	Tags map[string]string
+	// existing node addresses that any new node can join. the new node
+	// will connect to one none in the define addresses and then broadcast
+	// its presence to the other nodes through gossiping
+	StartJoinAddrs []string
+}
+
+func (m *Membership) setupSerf() error {
+	addr, err := net.ResolveTCPAddr("tcp", m.BindAddr)
+	if err != nil {
+		return err
+	}
+	config := serf.DefaultConfig()
+	config.Init()
+
+	// include current node membership details for gossiping
+	config.MemberlistConfig.BindAddr = addr.IP.String()
+	config.MemberlistConfig.BindPort = addr.Port
+
+	m.events = make(chan serf.Event)
+	config.EventCh = m.events
+
+	// key value metadata tags
+	config.Tags = m.Tags
+	config.NodeName = m.NodeName
+
+	// create service discovery instance
+	m.serf, err = serf.Create(config)
+	if err != nil {
+		return err
+	}
+
+	// handle events
+	go m.eventHandler()
+	if m.StartJoinAddrs != nil {
+		// join an existing cluster
+		if _, err = m.serf.Join(m.StartJoinAddrs, true); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// Handler represents a component in the service that needs to know
+// when a server joins or leaves the cluster
+type Handler interface {
+	Join(name, addr string) error
+	Leave(name string) error
+}
+
+// eventHandler handles Join and Leave events for its members. it runs in an
+// endless loop to ensure that all events are delivered.
+func (m *Membership) eventHandler() {
+	for e := range m.events {
+		switch e.EventType() {
+		case serf.EventMemberJoin:
+			// broadcast event to all members. the current event may contain
+			// one or more members
+			for _, member := range e.(serf.MemberEvent).Members {
+				// skip broadcasting event to itself
+				if !m.isLocal(member) {
+					m.handleJoin(member)
+				}
+			}
+		case serf.EventMemberLeave:
+			for _, member := range e.(serf.MemberEvent).Members {
+				// skip broadcasting event to itself
+				if !m.isLocal(member) {
+					m.handleLeave(member)
+				}
+			}
+		}
+	}
+}
+
+// handleJoins adds a new member to the cluster with their names and
+// rpc address tags
+func (m *Membership) handleJoin(member serf.Member) {
+	if err := m.handler.Join(member.Name, member.Tags["rpc_addr"]); err != nil {
+		m.logError(err, "failed to join", member)
+	}
+}
+
+// handleJoins removes a member from the cluster with their name
+func (m *Membership) handleLeave(member serf.Member) {
+	if err := m.handler.Leave(member.Name); err != nil {
+		m.logError(err, "failed to leave", member)
+	}
+}
+
+// isLocal checks whether the given member is the current local node
+func (m *Membership) isLocal(member serf.Member) bool {
+	return m.serf.LocalMember().Name == member.Name
+}
+
+// Members return a snapshot of  all the current members in the cluster
+func (m *Membership) Members() []serf.Member {
+	return m.serf.Members()
+}
+
+// logError logs the given error message with the member's details
+func (m *Membership) logError(err error, msg string, member serf.Member) {
+	m.logger.Error(
+		msg, zap.Error(err), zap.String("name", member.Name), zap.String("rpc_addr", member.Tags["rpc_addr"]),
+	)
+}
